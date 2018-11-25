bad0288cbf7a91c804c23537b7d224c378bb00c5
Simon Balean <simonbalean@gmail.com>
2018-11-25 23:22:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array = array.join("")
+    elsif array.size == 2 
+    array = array.join(" and ")
+  else 
+    size_of_array = array.size
+    # array[0..size_of_array - 1]
+    last = array.last
+    array1 = array.pop
+    array1.join(", ")
+    array1 << ", and"
+    array1 << last
+    array = array1
+    #array.join(", ")
+  end
 end
