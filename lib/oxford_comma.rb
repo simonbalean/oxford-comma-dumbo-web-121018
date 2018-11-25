521cfa4ce2bd8e58a562bcf78c090ad0cf38df6f
Simon Balean <simonbalean@gmail.com>
2018-11-25 23:27:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,14 +4,6 @@ def oxford_comma(array)
     elsif array.size == 2 
     array = array.join(" and ")
   else 
-    size_of_array = array.size
-    # array[0..size_of_array - 1]
-    last = array.last
-    array1 = array.pop
-    array1.join(", ")
-    array1 << ", and"
-    array1 << last
-    array = array1
-    #array.join(", ")
+    array[-1]
   end
 end
